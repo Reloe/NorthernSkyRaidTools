69bfa840a9e33023fc62addf2d3e2b7a40a59b76
Reloe <Jerome.Bellefroid@gmail.com>
2025-11-21 01:55:31 +0100

    add subgroup support to reminders and refactor them a bit

--- Reminders.lua
+++ Reminders.lua
@@ -1,12 +1,62 @@
 local _, NSI = ... -- Internal namespace
 
-NSI.EncounterDetections = {
-   -- [123] = {0, 10, 15, 20} [EncounterID] = {P1, P2, P3, etc} First is always 0 because it is for P1 which we start in.
-}
+function NSI:AddToReminder(text, phase, countdown, glowunit, sound, time, spellID, dur, TTS)
+    self.ProcessedAssigns = self.ProcessedAssigns or {}
+    spellID = spellID and tonumber(spellID)
+    -- convert to booleans
+    if TTS == "true" then TTS = true end
+    if TTS == "false" then TTS = false end
+    -- default to user settings if not overwritten by the reminders
+    if TTS == nil then 
+        if spellID then
+            TTS = NSRT.ReminderSettings.SpellTTS
+        else
+            TTS = NSRT.ReminderSettings.TextTTS
+        end
+    end            
+    if dur == nil then 
+        if spellID then
+             dur = NSRT.ReminderSettings.SpellDuration 
+        else
+            dur = NSRT.ReminderSettings.TextDuration 
+        end
+    end
+    if countdown == nil then
+        if spellID then
+            countdown = NSRT.ReminderSettings.SpellCountdown
+        else
+            countdown = NSRT.ReminderSettings.TextCountdown
+        end
+        if countdown == 0 then countdown = false end
+     end
+    phase = phase and tonumber(phase)
+    if not phase then phase = 1 end
+    if text then text = text:gsub("{rt(%d)}", "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_%1:0|t") end                    
+    if NSRT.ReminderSettings.SpellName and spellID then -- display spellname if text is empty, also make TTS that spellname
+        local spell = C_Spell.GetSpellInfo(spellID) 
+        if spell and not text then 
+            text = spell.name or ""
+            TTS = TTS and type(TTS) ~= "string" and spell.name
+        end 
+    end
+    if TTS and text and type(TTS) == "boolean" then
+        TTS = text
+    end
+    if TTS and type(TTS) ~= "string" and spellID then -- TTS is enabled but it's still empty, which means text was empty so we should play the spellname TTS instead
+        local spell = C_Spell.GetSpellInfo(spellID)
+        TTS = spell and spell.name
+    end
+    self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}    
+    table.insert(self.ProcessedAssigns[phase], {phase = phase, id = #self.ProcessedAssigns[phase]+1, countdown = countdown and tonumber(countdown), glowunit = glowunit, sound = sound, time = tonumber(time), text = text, TTS = TTS, spellID = spellID and tonumber(spellID), dur = dur or 8})      
+end
 
 function NSI:ProcessAssigns()
+    self.ProcessedAssigns = {}
     if self.Assigns and self.Assigns ~= "" then
-        self.ProcessedAssigns = {}
+        local subgroup = self:GetSubGroup("player")        
+        local specid = C_SpecializationInfo.GetSpecializationInfo(C_SpecializationInfo.GetSpecialization())
+        local pos = self.spectable[specid]
+        pos = (pos <= 19 and pos >= 7 and "meleedps") or (pos <= 33 and pos >= 20 and "rangeddps")
         for line in self.Assigns:gmatch('[^\r\n]+') do
             if line:find("EncounterID:") then
                 local encID = line:match("EncounterID:(%d+)")
@@ -20,17 +70,15 @@ function NSI:ProcessAssigns()
             local time = line:match("time:(%d*%.?%d+)")
             local text = line:match("text:([^;]+)")
             local spellID = line:match("spellid:(%d+)")
-            if time and tag and (text or spellID) then
+            if time and tag and subgroup and (text or spellID) then
                 tag = strlower(tag)
-                local specid = C_SpecializationInfo.GetSpecializationInfo(C_SpecializationInfo.GetSpecialization())
-                local pos = self.spectable[specid]
-                pos = (pos <= 19 and pos >= 7 and "meleedps") or (pos <= 33 and pos >= 20 and "rangeddps")
                 if tag == "everyone" or 
                 tag:match(strlower(UnitName("player"))) or 
-                tag:match(strlower(UnitGroupRolesAssigned("player"))) or 
                 tag:match(strlower(NSAPI:GetName("player", "GlobalNickNames"))) or 
+                tag:match(strlower(UnitGroupRolesAssigned("player"))) or 
                 tag:match(specid) or
                 tag:match(strlower(select(2, UnitClass("player")))) or
+                tag:match("group"..subgroup) or 
                 (pos and tag:match(pos))
                 then                         
                     local phase = line:match("ph:(%d+)")
@@ -39,49 +87,7 @@ function NSI:ProcessAssigns()
                     local dur = line:match("dur:(%d+)")
                     local sound = line:match("sound:([^;]+)")
                     local glowunit = line:match("glowunit:([^;]+)")
-                    spellID = spellID and tonumber(spellID)
-                    -- convert to booleans
-                    if TTS == "true" then TTS = true end
-                    if TTS == "false" then TTS = false end
-                    -- default to user settings if not overwritten by the reminders
-                    if TTS == nil then 
-                        if spellID then
-                            TTS = NSRT.ReminderSettings.SpellTTS
-                        else
-                            TTS = NSRT.ReminderSettings.TextTTS
-                        end                    
-                    end
-                    if dur == nil then 
-                        if spellID then
-                            dur = NSRT.ReminderSettings.SpellDuration 
-                        else
-                            dur = NSRT.ReminderSettings.TextDuration 
-                        end
-                    end
-                    if countdown == nil then
-                        if spellID then
-                            countdown = NSRT.ReminderSettings.SpellCountdown
-                        else
-                            countdown = NSRT.ReminderSettings.TextCountdown
-                        end
-                        if countdown == 0 then countdown = false end
-                    end
-                    phase = phase and tonumber(phase)
-                    if not phase then phase = 1 end
-                    if text then text = text:gsub("{rt(%d)}", "|TInterface\\TargetingFrame\\UI-RaidTargetingIcon_%1:0|t") end                    
-                    if NSRT.ReminderSettings.SpellName and spellID then -- display spellname if text is empty, also make TTS that spellname
-                        local spell = C_Spell.GetSpellInfo(spellID) 
-                        if spell and not text then 
-                            text = spell.name or ""
-                            TTS = TTS and type(TTS) ~= "string" and spell.name
-                        end 
-                    end
-                    if TTS and type(TTS) ~= "string" and spellID then -- TTS is enabled but it's still empty, which means text was empty so we should play the spellname TTS instead
-                        local spell = C_Spell.GetSpellInfo(spellID)
-                        TTS = spell and spell.name
-                    end
-                    self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}    
-                    table.insert(self.ProcessedAssigns[phase], {phase = phase, id = #self.ProcessedAssigns[phase]+1, countdown = countdown and tonumber(countdown), glowunit = glowunit, sound = sound, time = tonumber(time), text = text, TTS = TTS, spellID = spellID and tonumber(spellID), dur = dur or 8})
+                    self:AddToReminder(text, phase, countdown, glowunit, sound, time, spellID, dur, TTS)
                 end
             end
         end
@@ -206,7 +212,7 @@ function NSI:CreateIcon(info)
             self.AssignIcon[i].Border:SetBackdropBorderColor(0, 0, 0, 1)
             self.AssignIcon[i].Text = self.AssignIcon[i]:CreateFontString(nil, "OVERLAY", "GameFontNormal")
             self.AssignIcon[i].Text:SetPoint("LEFT", self.AssignIcon[i], "RIGHT", s.xTextOffset, s.yTextOffset)
-            self.AssignIcon[i].Text:SetFont(s.Font, s.FontSize, "OUTLINE")
+            self.AssignIcon[i].Text:SetFont(self.LSM:Fetch("font", s.Font), s.FontSize, "OUTLINE")
             self.AssignIcon[i].Text:SetShadowColor(0, 0, 0, 1)
             self.AssignIcon[i].Text:SetShadowOffset(0, 0)
             self.AssignIcon[i].Text:SetTextColor(1, 1, 1, 1)
@@ -425,7 +431,11 @@ function NSI:StartReminders(phase)
     for i, v in ipairs(self.ProcessedAssigns[phase]) do
         local time = math.max(v.time-v.dur, 0)
         self.ReminderTimer[i] = C_Timer.NewTimer(time, function()
-            self:DisplayReminder(v)
+            if self:Restricted() or NSRT.Settings["Debug"] then 
+                self:DisplayReminder(v) 
+            else
+                self:HideAllReminders()
+            end
         end)
     end
 end
@@ -472,15 +482,21 @@ function NSI:ImportReminder(name, values)
     -- NSI:UpdateReminderList()
 end
 
+NSI.EncounterDetections = {
+   [3182] = {0, 3, 3, 3, 3, 3, 3, 3}, -- starting new phase when at least 3 timers get removed which should only happen at the end of p1
+}
+
+
 function NSI:DetectPhaseChange(e)
     local now = GetTime()
-    if NSRT.Settings["Debug"] and NSRT.Settings["DebugLogs"] then
+    if NSRT.Settings["Debug"] and NSRT.Settings["DebugLogs"] and self.Phase then
         self.TimeLinesDebug = self.TimeLinesDebug or {}
         self.TimeLinesDebug.EncounterID= self.TimeLinesDebug.EncounterID or self.EncounterID
         self.TimeLinesDebug[self.Phase] = self.TimeLinesDebug[self.Phase] or {}
         self.TimeLinesDebug[self.Phase][e] = self.TimeLinesDebug[self.Phase][e] or {}
         table.insert(self.TimeLinesDebug[self.Phase][e], now)
     end
+    if e == "ENCOUNTER_TIMELINE_EVENT_ADDED" and self.EncounterID == 3182 then return end -- starting new phase only on timer being removed for this boss
     local needed = self.Timelines and self.PhaseSwapTime and (now > self.PhaseSwapTime+5) and self.EncounterID and self.Phase and self.EncounterDetections[self.EncounterID] and self.EncounterDetections[self.EncounterID][self.Phase+1]
     if needed then
         table.insert(self.Timelines, now+1)
@@ -497,12 +513,6 @@ function NSI:DetectPhaseChange(e)
                 end
             end           
         end
-    end
-end
-
-function NSAPI:DebugNextPhase(num)
-    for i=1, num do
-        NSI:EventHandler("ENCOUNTER_TIMELINE_EVENT_ADDED")
     end
 end
 
@@ -531,6 +541,7 @@ function NSI:HideGlow(unit, id)
 end
 
 function NSI:StoreFrames(init)
+    if self:Restricted() then return end
     self.RaidFrames = {}
     if init then
         local MyFrame = self.LGF.GetUnitFrame("player")
@@ -547,9 +558,15 @@ function NSI:StoreFrames(init)
     end
 end
 
+function NSAPI:DebugNextPhase(num)
+    for i=1, num do
+        NSI:EventHandler("ENCOUNTER_TIMELINE_EVENT_ADDED")
+    end
+end
+
 function NSAPI:DebugReminder(EncounterID, startnow)
     if NSRT.Settings["Debug"] then
-        local text = "EncounterID:"..EncounterID.."\nph:1;time:10;tag:Relowindi;text:Stack on {rt7};sound:Stack;countdown:3;TTS:Stack on Red;dur:8;"
+        local text = "EncounterID:"..EncounterID.."\nph:1;time:10;tag:Senfi Group1;text:Stack on {rt7};sound:Stack;countdown:3;TTS:Stack on Red;dur:8;"
         text = text.."\n".."time:15;tag:monk;TTS:true;spellid:115203;"
         text = text.."\n".."ph:1;time:25;tag:everyone;text:Lust on Reloe;glowunit:Reloe;spellid:116841;TTS:true;"
         text = text.."\n".."ph:2;time:10;tag:Reloe;text:Spread;TTS:true;dur:10;"
@@ -562,25 +579,10 @@ function NSAPI:DebugReminder(EncounterID, startnow)
         NSI:ProcessAssigns()
         if startnow then
             NSI:EventHandler("ENCOUNTER_START", true, true, EncounterID)
-            C_Timer.After(20, function()
-                NSAPI:DebugNextPhase(10)
+            C_Timer.After(40, function()
+                -- NSAPI:DebugNextPhase(10)
             end)
         end
-    end
-end
-
-function NSI:AddAssignments(id)
-    if NSRT.Settings["Debug"] and (id == 2900 or id == 3306 or id == 3182 or id == 3176 or id == 3177 or id == 3179 or id == 3178 or id == 3180) then -- all raid tests debug & 1st Boss Cinderbrew
-        local phase, countdown, glowunit, sound, time, spellID, dur = 1, 3, false, false, 20, false, 10
-        local subgroup = math.random(1, 4)
-        local text = subgroup <= 2 and "|cFF00FF00SOAK|r" or "|cFFFF0000DON'T SOAK|r"
-        local TTS = subgroup <= 2 and "Soak" or "Don't Soak"   
-        self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
-        table.insert(self.ProcessedAssigns[phase], {phase = phase, id = #self.ProcessedAssigns[phase]+1, countdown = countdown, glowunit = glowunit, sound = sound, time = time, text = text, TTS = TTS, spellID = spellID, dur = dur})
-        phase = 2
-        text = subgroup <= 2 and "|cFF00FF00Go Left" or "|cFFFF0000Go Right"
-        TTS = subgroup <= 2 and "Go Left" or "Go Right"self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
-        table.insert(self.ProcessedAssigns[phase], {phase = phase, id = #self.ProcessedAssigns[phase]+1, countdown = countdown, glowunit = glowunit, sound = sound, time = time, text = text, TTS = TTS, spellID = spellID, dur = dur})
     end
 end
 
@@ -594,3 +596,36 @@ end
 -- /run NSAPI:DebugReminder(2900)
 -- Debug has to be run before pulling. If player isn't raidlead it needs to be done after ready check.
 -- /run NSAPI:DebugReminder(2900, true) to test outside of combat
+
+
+function NSI:AddAssignments(id)
+    if not NSRT.Settings["Debug"] then return end
+    if id == 3182 then
+        local phase, countdown, glowunit, sound, time, spellID, dur, TTS = 1, 3, "Reloe", false, 10, 115203, 8
+        local text = false
+        self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
+        self:AddToReminder(text, phase, countdown, glowunit, sound, time, spellID, dur, TTS)
+
+        local phase, countdown, glowunit, sound, time, spellID, dur, TTS = 1, false, false, "Soak", 20, false, 8
+        local text = "Soak"
+        self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
+        self:AddToReminder(text, phase, countdown, glowunit, sound, time, spellID, dur, TTS)
+
+        local phase, countdown, glowunit, sound, time, spellID, dur, TTS = 1, false, false, false, 25, false, 8
+        local text = "Balls"
+        self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
+        self:AddToReminder(text, phase, countdown, glowunit, sound, time, spellID, dur, TTS)
+        
+    elseif (id == 2900 or id == 3306 or id == 3176 or id == 3177 or id == 3179 or id == 3178 or id == 3180) then -- all raid tests debug & 1st Boss Cinderbrew
+        local phase, countdown, glowunit, sound, time, spellID, dur = 1, 3, false, false, 20, false, 10
+        local subgroup = math.random(1, 4)
+        local text = subgroup <= 2 and "|cFF00FF00SOAK|r" or "|cFFFF0000DON'T SOAK|r"
+        local TTS = subgroup <= 2 and "Soak" or "Don't Soak"   
+        self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
+        table.insert(self.ProcessedAssigns[phase], {phase = phase, id = #self.ProcessedAssigns[phase]+1, countdown = countdown, glowunit = glowunit, sound = sound, time = time, text = text, TTS = TTS, spellID = spellID, dur = dur})
+        phase = 2
+        text = subgroup <= 2 and "|cFF00FF00Go Left" or "|cFFFF0000Go Right"
+        TTS = subgroup <= 2 and "Go Left" or "Go Right"self.ProcessedAssigns[phase] = self.ProcessedAssigns[phase] or {}
+        table.insert(self.ProcessedAssigns[phase], {phase = phase, id = #self.ProcessedAssigns[phase]+1, countdown = countdown, glowunit = glowunit, sound = sound, time = time, text = text, TTS = TTS, spellID = spellID, dur = dur})
+    end
+end
