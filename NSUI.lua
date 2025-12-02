e5c85153ba5f52417f79569bcc955911532bd3f5
Reloe <Jerome.Bellefroid@gmail.com>
2025-12-02 05:53:07 +0100

    add assignment UI

--- NSUI.lua
+++ NSUI.lua
@@ -15,7 +15,8 @@ local TABS_LIST = {
     { name = "Versions",  text = "Versions" },
     { name = "WeakAuras", text = "WeakAuras" },
     { name = "SetupManager", text = "Setup Manager"},
-    { name = "Reminders", text = "Reminders"}
+    { name = "Reminders", text = "Reminders"},
+    { name = "Assignments", text = "Assignments"}
 }
 local authorsString = "By Reloe & Rav"
 
@@ -1098,6 +1099,7 @@ function NSUI:Init()
     local weakaura_tab = tabContainer:GetTabFrameByName("WeakAuras")
     local setupmanager_tab = tabContainer:GetTabFrameByName("SetupManager")
     local reminder_tab = tabContainer:GetTabFrameByName("Reminders")
+    local assignments_tab = tabContainer:GetTabFrameByName("Assignments")
 
     -- generic text display
     local generic_display = CreateFrame("Frame", "NSUIGenericDisplay", UIParent, "BackdropTemplate")
@@ -3003,6 +3005,68 @@ Press 'Enter' to hear the TTS]],
 
         },
     }
+    local assignments_options1_table = {        
+        {
+            type = "label",
+            get = function() return "Vaelgor & Ezzorak" end,
+            text_template = DF:GetTemplate("font", "ORANGE_FONT_TEMPLATE"),
+        },
+        {
+            type = "toggle",
+            boxfirst = true,
+            name = "Gloom Soaks",
+            desc = "Automatically tells Group 2 to soak the first Cast of Gloom and Group 3 to soak the second cast",
+            get = function() return NSRT.AssignmentSettings[3178] and NSRT.AssignmentSettings[3178].Soaks end,
+            set = function(self, fixedparam, value)
+                NSRT.AssignmentSettings[3178] = NSRT.AssignmentSettings[3178] or {}
+                NSRT.AssignmentSettings[3178].Soaks = value
+                self.Assignments = NSRT.AssignmentSettings
+            end,
+            nocombat = true,
+        },
+        {
+            type = "label",
+            get = function() return "Lightblinded Vanguard" end,
+            text_template = DF:GetTemplate("font", "ORANGE_FONT_TEMPLATE"),
+        },
+        {
+            type = "toggle",
+            boxfirst = true,
+            name = "Execution Sentence",
+            desc = "Automatically assigns players to Star, Orange, Triangle and Purple for Execution Sentence. Melee are preferred for Star/Orange, Ranged for Triangle/Purple. You should be putting down World Markers for this.",
+            get = function() return NSRT.AssignmentSettings[3180] and NSRT.AssignmentSettings[3180].Soaks end,
+            set = function(self, fixedparam, value)
+                NSRT.AssignmentSettings[3180] = NSRT.AssignmentSettings[3180] or {}
+                NSRT.AssignmentSettings[3180].Soaks = value
+                self.Assignments = NSRT.AssignmentSettings
+            end,
+            nocombat = true,
+        },
+        {
+            type = "label",
+            get = function() return "Chimaerus" end,
+            text_template = DF:GetTemplate("font", "ORANGE_FONT_TEMPLATE"),
+        },
+        {
+            type = "toggle",
+            boxfirst = true,
+            name = "Alndust Upheaval",
+            desc = "Automatically tells Groups 1&2 to soak the first Cast of Alndust Upheaval and Group 3&4 to soak the second cast",
+            get = function() return NSRT.AssignmentSettings[3306] and NSRT.AssignmentSettings[3306].Soaks end,
+            set = function(self, fixedparam, value)
+                NSRT.AssignmentSettings[3306] = NSRT.AssignmentSettings[3306] or {}
+                NSRT.AssignmentSettings[3306].Soaks = value
+                --
+                NSRT.AssignmentSettings[1024] = NSRT.AssignmentSettings[1024] or {}
+                NSRT.AssignmentSettings[1024].Soaks = value
+                self.Assignments = NSRT.AssignmentSettings
+            end,
+            nocombat = true,
+        },
+
+
+    }
+
 
     -- Build options menu for each tab
     DF:BuildMenu(general_tab, general_options1_table, 10, -100, window_height - 10, false, options_text_template,
@@ -3023,6 +3087,9 @@ Press 'Enter' to hear the TTS]],
     DF:BuildMenu(reminder_tab, reminder_options1_table, 10, -100, window_height - 10, false, options_text_template,
         options_dropdown_template, options_switch_template, true, options_slider_template, options_button_template,
         reminder_callback)
+    DF:BuildMenu(assignments_tab, assignments_options1_table, 10, -100, window_height - 10, false, options_text_template,
+        options_dropdown_template, options_switch_template, true, options_slider_template, options_button_template,
+        assignments_callback)
 
     -- Add SUF Setup guide tooltip button thingy
 
