c636ae56962c214d6d47992f208ddbcad0cd8554
MistaSweeD <markus@warband.io>
2026-01-16 12:43:07 +0100

    update all boss timeline files to new format
    - remove phase name and color fields (now optional)
    - remove important field from abilities
    - use integer durations instead of floats
    - remove duplicate Belo'ren data from BossTimelines.lua

--- BossTimelines/Vorasius.lua
+++ BossTimelines/Vorasius.lua
@@ -7,14 +7,14 @@ local _, NSI = ... -- Internal namespace
 NSI.BossTimelines[3177] = {
     duration = 400,
     phases = {
-        [1] = {name = "Single Phase", start = 0, color = {0.23, 0.51, 0.96}},
+        [1] = {start = 0},
     },
     abilities = {
-        {name = "Primordial Roar", spellID = 1260052, category = "damage", phase = 1, times = {12, 132, 253}, duration = 3.0, important = true},
-        {name = "Smashing Frenzy", spellID = 1241836, category = "tank", phase = 1, times = {17, 22, 27, 32, 36, 41, 46, 51, 68, 73, 78, 83, 137, 142, 147, 152, 157, 162, 166, 171, 191, 196, 201, 205, 258, 263, 267, 272, 277, 282, 287, 292, 314, 319, 328}, duration = 2.0, important = false},
-        {name = "Parasite Expulsion", spellID = 1254199, category = "movement", phase = 1, times = {60, 182, 305}, duration = 4.0, important = false},
-        {name = "Fixate", spellID = 1254113, category = "movement", phase = 1, times = {67, 189, 313}, duration = 10.0, important = false},
-        {name = "Blisterburst", spellID = 1259186, category = "damage", phase = 1, times = {77, 199, 323}, duration = 2.0, important = true},
-        {name = "Void Breath", spellID = 1256855, category = "damage", phase = 1, times = {101, 222, 343}, duration = 5.0, important = true},
+        {name = "Primordial Roar", spellID = 1260052, category = "damage", phase = 1, times = {12, 132, 253}, duration = 3},
+        {name = "Smashing Frenzy", spellID = 1241836, category = "tank", phase = 1, times = {17, 22, 27, 32, 36, 41, 46, 51, 68, 73, 78, 83, 137, 142, 147, 152, 157, 162, 166, 171, 191, 196, 201, 205, 258, 263, 267, 272, 277, 282, 287, 292, 314, 319, 328}, duration = 2},
+        {name = "Parasite Expulsion", spellID = 1254199, category = "movement", phase = 1, times = {60, 182, 305}, duration = 4},
+        {name = "Fixate", spellID = 1254113, category = "movement", phase = 1, times = {67, 189, 313}, duration = 10},
+        {name = "Blisterburst", spellID = 1259186, category = "damage", phase = 1, times = {77, 199, 323}, duration = 2},
+        {name = "Void Breath", spellID = 1256855, category = "damage", phase = 1, times = {101, 222, 343}, duration = 5},
     },
 }
