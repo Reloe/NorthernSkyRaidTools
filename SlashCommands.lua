a6f833a75e6e940667c9932604ed89d099555d01
Reloe <Jerome.Bellefroid@gmail.com>
2026-01-26 23:47:01 +0100

    update slash commands with a /ns help option as well. If an unrecognized command is used, the user will be told about /help as well

--- SlashCommands.lua
+++ SlashCommands.lua
@@ -3,21 +3,9 @@ local _, NSI = ... -- Internal namespace
 SLASH_NSUI1 = "/ns"
 SLASH_NSUI2 = "/nsrt"
 SlashCmdList["NSUI"] = function(msg)
-    if msg == "anchor" then
-        if NSI.NSUI.externals_anchor:IsShown() then
-            NSI.NSUI.externals_anchor:Hide()
-        else
-            NSI.NSUI.externals_anchor:Show()
-        end
-    elseif msg == "test" then
-        NSI:DisplayExternal(nil, GetUnitName("player"))
-    elseif msg == "wipe" then
+    if msg == "wipe" then
         wipe(NSRT)
         ReloadUI()
-    elseif msg == "sync" then
-        NSI:NickNamesSyncPopup(GetUnitName("player"), "yayayaya")
-    elseif msg == "display" then
-        NSI:DisplayText("Display text", 8)
     elseif msg == "debug" then
         if NSRT.Settings["Debug"] then
             NSRT.Settings["Debug"] = false
@@ -44,39 +32,51 @@ SlashCmdList["NSUI"] = function(msg)
         else
             NSUI.personal_reminders_frame:Hide()
         end
-    elseif msg == "note" or msg == "n" then
+    elseif msg == "note" or msg == "n" then -- Toggle Showing/Hiding ALL Notes
+        local ShouldHide = NSRT.ReminderSettings.ShowReminderFrame and NSRT.ReminderSettings.ShowPersonalReminderFrame and NSRT.ReminderSettings.ShowExtraReminderFrame
+        NSRT.ReminderSettings.ShowReminderFrame = not ShouldHide
+        NSRT.ReminderSettings.ShowPersonalReminderFrame = not ShouldHide
+        NSRT.ReminderSettings.ShowExtraReminderFrame = not ShouldHide
+        NSI:ProcessReminder()
+        NSI:UpdateReminderFrame(false, true)
+    elseif msg == "anote" or msg == "an" or msg == "snote" or msg == "sn" then -- Toggle the "All Reminders Note"
         NSRT.ReminderSettings.ShowReminderFrame = not NSRT.ReminderSettings.ShowReminderFrame
         NSI:ProcessReminder()
-        NSI:UpdateReminderFrame(false)
-    elseif msg == "pnote" or msg == "pn" then
+        NSI:UpdateReminderFrame()
+    elseif msg == "pnote" or msg == "pn" then -- Toggle the "Personal Reminders Note"
         NSRT.ReminderSettings.ShowPersonalReminderFrame = not NSRT.ReminderSettings.ShowPersonalReminderFrame
         NSI:ProcessReminder()
         NSI:UpdateReminderFrame(true)
-    elseif msg == "clear" or msg == "c" then
+    elseif msg == "tnote" or msg == "tn" then -- Toggle the "Text Note"
+        NSI:UpdateReminderFrame(false, false, true)
+    elseif msg == "clear" or msg == "c" then -- Clear Active Reminder
         NSRT.ActiveReminder = nil
         NSI.Reminder = ""
         NSI:ProcessReminder()
-
         NSI:UpdateReminderFrame(false, true)
-    elseif msg == "timeline" or msg == "tl"then
+    elseif msg == "pclear" or msg == "pc" then -- Clear Active Personal Reminder
+        NSRT.ActivePersonalReminder = nil
+        NSI.PersonalReminder = ""
+        NSI:ProcessReminder()
+        NSI:UpdateReminderFrame(false, true)
+    elseif msg == "timeline" or msg == "tl" then
         NSI:ToggleTimelineWindow()
     elseif msg == "help" then
-        print("|cFF00FFFFNSRT|r Available commands:")
-        print("  |cFFFFFF00/ns anchor|r - Toggle externals anchor visibility")
-        print("  |cFFFFFF00/ns test|r - Display external test")
-        print("  |cFFFFFF00/ns wipe|r - Wipe NSRT settings and reload UI")
-        print("  |cFFFFFF00/ns sync|r - Show nickname sync popup (test)")
-        print("  |cFFFFFF00/ns display|r - Display test text")
-        print("  |cFFFFFF00/ns debug|r - Toggle debug mode")
-        print("  |cFFFFFF00/ns cd|r - Toggle cooldowns frame")
-        print("  |cFFFFFF00/ns reminders|r or |cFFFFFF00/ns r|r - Toggle reminders frame")
-        print("  |cFFFFFF00/ns preminders|r or |cFFFFFF00/ns pr|r - Toggle personal reminders frame")
-        print("  |cFFFFFF00/ns note|r or |cFFFFFF00/ns n|r - Toggle reminder frame display")
-        print("  |cFFFFFF00/ns pnote|r or |cFFFFFF00/ns pn|r - Toggle personal reminder frame display")
-        print("  |cFFFFFF00/ns clear|r or |cFFFFFF00/ns c|r - Clear active reminder")
-        print("  |cFFFFFF00/ns timeline|r or |cFFFFFF00/ns tl|r - Toggle timeline window")
-        print("  |cFFFFFF00/ns help|r or |cFFFFFF00/ns h|r - Show this help message")
-        print("  |cFFFFFF00/ns|r (no command) - Toggle options window")
+        print("|cFF00FFFFNSRT|r Available commands: (either '/ns' or 'nsrt' work)\n")
+        print("  |cFF00FFFF/ns debug|r - Toggle debug mode - mainly used for development")
+        print("  |cFF00FFFF/ns wipe|r - Wipe ALL NSRT settings and reload UI")
+        print("  |cFF00FFFF/ns cd|r - Toggle cooldowns frame")
+        print("  |cFF00FFFF/ns clear|r or |cFF00FFFF/ns c|r - Clear active reminder")
+        print("  |cFF00FFFF/ns pclear|r or |cFF00FFFF/ns pc|r - Clear active personal reminder")
+        print("  |cFF00FFFF/ns reminders|r or |cFF00FFFF/ns r|r - Shortcut to Shared Reminders List")
+        print("  |cFF00FFFF/ns preminders|r or |cFF00FFFF/ns pr|r - Shortcut to Personal Reminders List")
+        print("  |cFF00FFFF/ns note|r or |cFF00FFFF/ns n|r - Toggle all notes (all reminders, personal reminders, and text note)")
+        print("  |cFF00FFFF/ns anote|r or |cFF00FFFF/ns an|r or |cFF00FFFF/ns snote|r or |cFF00FFFF/ns sn|r - Toggle Shared Reminders note")
+        print("  |cFF00FFFF/ns pnote|r or |cFF00FFFF/ns pn|r - Toggle Personal Reminders note")
+        print("  |cFF00FFFF/ns tnote|r or |cFF00FFFF/ns tn|r - Toggle Text note")
+        print("  |cFF00FFFF/ns timeline|r or |cFF00FFFF/ns tl|r - Toggle timeline window")
+    elseif msg then
+        print("|cFF00FFFFNSRT|r Unknown command. Type |cFF00FFFF/ns help|r for a list of commands.")
     else
         NSI.NSUI:ToggleOptions()
     end
