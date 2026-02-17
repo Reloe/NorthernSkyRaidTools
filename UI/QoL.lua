e5cbd338dabb1c8065663e17bb63497cfae86b69
Reloe <Jerome.Bellefroid@gmail.com>
2026-02-18 00:51:45 +0100

    Add Frame Creation, Text Display and Preview to QoL

--- UI/QoL.lua
+++ UI/QoL.lua
@@ -4,6 +4,80 @@ local DF = _G["DetailsFramework"]
 local Core = NSI.UI.Core
 local NSUI = Core.NSUI
 
+function NSI:UpdateQoLTextDisplay()
+    if self.IsQoLTextPreview then return end
+    self:CreateQoLTextDisplay()
+    local F = self.NSRTFrame.QoLText
+    F:ClearAllPoints()
+    F:SetPoint(NSRT.QoL.TextDisplay.Anchor, self.NSRTFrame, NSRT.QoL.TextDisplay.relativeTo, NSRT.QoL.TextDisplay.xOffset, NSRT.QoL.TextDisplay.yOffset)
+    F.text:SetFont(self.LSM:Fetch("font", NSRT.Settings.GlobalFont), NSRT.QoL.TextDisplay.FontSize, "OUTLINE")
+    local text = ""
+    local now = GetTime()
+    for _, v in pairs(self.QoLTextDisplays or {}) do -- table structure: {SettingsName = string, text = string}
+        if NSRT.QoL[v.SettingsName] then
+            text = text..v.text.."\n"
+        end
+    end
+    F.text:SetText(text)
+    F:SetSize(F.text:GetStringWidth(), F.text:GetStringHeight())
+end
+
+function NSI:CreateQoLTextDisplay()
+    if self.NSRTFrame.QoLText then return end
+    self.NSRTFrame.QoLText = CreateFrame("Frame", nil, self.NSRTFrame, "BackdropTemplate")
+    self.NSRTFrame.QoLText.text = self.NSRTFrame.QoLText:CreateFontString(nil, "OVERLAY", "GameFontNormal")
+    local F = self.NSRTFrame.QoLText
+    F:SetPoint(NSRT.QoL.TextDisplay.Anchor, self.NSRTFrame, NSRT.QoL.TextDisplay.relativeTo, NSRT.QoL.TextDisplay.xOffset, NSRT.QoL.TextDisplay.yOffset)
+    F:SetFrameStrata("DIALOG")
+    F.text:SetFont(self.LSM:Fetch("font", NSRT.Settings.GlobalFont), NSRT.QoL.TextDisplay.FontSize, "OUTLINE")
+    F.text:SetPoint("CENTER", F, "CENTER", 0, 0)
+    F.text:SetJustifyH("LEFT")
+    F.text:SetTextColor(1, 1, 1, 1)
+    F.Border = CreateFrame("Frame", nil, F, "BackdropTemplate")
+    F.Border:SetPoint("TOPLEFT", F, "TOPLEFT", -6, 6)
+    F.Border:SetPoint("BOTTOMRIGHT", F, "BOTTOMRIGHT", 6, -6)
+    F.Border:SetBackdrop({
+            bgFile = "Interface\\Buttons\\WHITE8x8",
+            tileSize = 0,
+            edgeFile = "Interface\\Buttons\\WHITE8x8",
+            edgeSize = 2,
+        })
+    F.Border:SetBackdropBorderColor(1, 1, 1, 1)
+    F.Border:SetBackdropColor(0, 0, 0, 0)
+    F.Border:Hide()
+    F.Border:SetFrameStrata("DIALOG")
+    F:SetScript("OnDragStart", function(self)
+        self:StartMoving()
+    end)
+    F:SetScript("OnDragStop", function(Frame)
+        self:StopFrameMove(Frame, NSRT.QoL.TextDisplay)
+    end)
+end
+
+function NSI:ToggleQoLTextPreview()
+    if self.IsQoLTextPreview then
+        self:CreateQoLTextDisplay()
+        local PrevieWTexts = {
+        "This is a preview of the QoL Text Display.",
+        "You can move this display around and change its font size in the options.",
+        "All enabled Text Displays will show here.",
+        }
+        local text = ""
+        for _, v in ipairs(PrevieWTexts) do -- table structure: {enabled = bool, text = string}
+            text = text..v.."\n"
+        end
+        local F = self.NSRTFrame.QoLText
+        F.text:SetText(text)
+        F:SetSize(F.text:GetStringWidth(), F.text:GetStringHeight()*1.5)
+        self:ToggleMoveFrames(F, true)
+    else
+        self:ToggleMoveFrames(self.NSRTFrame.QoLText)
+        self:UpdateQoLTextDisplay()
+    end
+end
+
+
+
 
 -- Export to namespace
 NSI.UI = NSI.UI or {}
