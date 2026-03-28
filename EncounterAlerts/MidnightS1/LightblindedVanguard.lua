8ad5335eda9b835946f5a3e9a8d6f55bdd11fc49
Reloe <Jerome.Bellefroid@gmail.com>
2026-03-28 21:39:51 +0100

    Update LightblindedVanguard.lua

--- EncounterAlerts/MidnightS1/LightblindedVanguard.lua
+++ EncounterAlerts/MidnightS1/LightblindedVanguard.lua
@@ -45,7 +45,7 @@ NSI.EncounterAlertStart[encID] = function(self, id) -- on ENCOUNTER_START
         local timers = {
             [0] = {},
             [15] = {147.3, 324.4},
-            [16] = {34.4, 142.6, 192.5, 302},
+            [16] = {34.4, 142.6, 192.5, 302, 352, 461.5},
         }
         self.AlertTimers = self.AlertTimers or {}
         local dur = id == 16 and 20 or 15
