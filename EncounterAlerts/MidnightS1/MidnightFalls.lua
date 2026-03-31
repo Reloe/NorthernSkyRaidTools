b644c497a717143732ab1037405125a6c4058a8c
Reloe <Jerome.Bellefroid@gmail.com>
2026-03-31 21:28:35 +0200

    add p1 Lu'ra Alerts and remove phase detection for now

--- EncounterAlerts/MidnightS1/MidnightFalls.lua
+++ EncounterAlerts/MidnightS1/MidnightFalls.lua
@@ -2,21 +2,68 @@ local _, NSI = ... -- Internal namespace
 
 local encID = 3183
 -- /run NSAPI:DebugEncounter(3183)
+NSI.EncounterAlertStart[encID] = function(self, id) -- on ENCOUNTER_START
+    if not NSRT.EncounterAlerts[encID] then
+        NSRT.EncounterAlerts[encID] = {enabled = false}
+    end
+    if NSRT.EncounterAlerts[encID].enabled then -- text, Type, spellID, dur, phase, encID
+        id = id or self:DifficultyCheck(14) or 0
+
+        local Alert = self:CreateDefaultAlert("Memory Game", "Text", nil, 8, 1, encID) -- Memory Game
+        local timers = {
+            [15] = {10, 80, 150}
+        }
+        for i, v in ipairs(timers[id] or {}) do
+            Alert.time = v
+            self:AddToReminder(Alert)
+        end
+
+        Alert.text = "Glaives"
+        timers = {
+            [15] = {37.3, 107, 177.3}
+        }
+        for i, v in ipairs(timers[id] or {}) do
+            Alert.time = v
+            self:AddToReminder(Alert)
+        end
+
+        Alert.text = "Beams"
+        timers = {
+            [15] = {41, 111.5}
+        }
+        for i, v in ipairs(timers[id] or {}) do
+            Alert.time = v
+            self:AddToReminder(Alert)
+        end
+
+        Alert.text = "Interrupts"
+        timers = {
+            [15] = {58, 129}
+        }
+        for i, v in ipairs(timers[id] or {}) do
+            Alert.time = v
+            self:AddToReminder(Alert)
+        end
+    end
+end
+
+local detectedDurations = {
+}
+
 NSI.DetectPhaseChange[encID] = function(self, e, info)
     local now = GetTime()
-    if e == "ENCOUNTER_TIMELINE_EVENT_ADDED" then
-        table.insert(self.Timelines, now)
-        local count = 0
-        for k, v in ipairs(self.Timelines) do
-            if now < v+0.1 then
-                count = count+1
-                if count >= 3 then -- default fallback phase transition detection, needs to be edited after seeing the encounter
-                    self.Phase = self.Phase+1
-                    self:StartReminders(self.Phase)
-                    self.PhaseSwapTime = now
-                    return
-                end
-            end
+    -- not checking REMOVED event by default but may be needed for some encounters
+    if e == "ENCOUNTER_TIMELINE_EVENT_REMOVED" or (not info) or (not self.PhaseSwapTime) or (not (now > self.PhaseSwapTime+5)) or (not self.EncounterID) or (not self.Phase) then return end
+    local difficultyID = select(3, GetInstanceInfo()) or 0
+    if (not difficultyID) or (not detectedDurations[difficultyID]) then return end
+    local phaseinfo = detectedDurations[difficultyID][1]
+    if phaseinfo and info.duration >= phaseinfo.time then
+        local newphase = phaseinfo.phase(self.Phase)
+        if newphase > self.Phase then
+            self.Phase = newphase
+            self:StartReminders(self.Phase)
+            self.PhaseSwapTime = now
+            RiftMadnessTimers()
         end
     end
 end
