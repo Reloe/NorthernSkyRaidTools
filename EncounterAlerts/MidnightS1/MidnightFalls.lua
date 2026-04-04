e4420b4b3f758abe6fb7984870b5df6d75ad805d
Reloe <Jerome.Bellefroid@gmail.com>
2026-04-04 16:55:14 +0200

    Update MidnightFalls.lua

--- EncounterAlerts/MidnightS1/MidnightFalls.lua
+++ EncounterAlerts/MidnightS1/MidnightFalls.lua
@@ -8,7 +8,6 @@ NSI.EncounterAlertStart[encID] = function(self, id) -- on ENCOUNTER_START
     end
     if NSRT.EncounterAlerts[encID].enabled then -- text, Type, spellID, dur, phase, encID
         id = id or self:DifficultyCheck(14) or 0
-
         local Alert = self:CreateDefaultAlert("Memory Game", "Text", nil, 6, 1, encID)
         local timers = {
             [15] = {10, 80, 150}
@@ -16,39 +15,39 @@ NSI.EncounterAlertStart[encID] = function(self, id) -- on ENCOUNTER_START
         self:AddRemindersFromTable(Alert, timers[id])
 
         local Alert = self:CreateDefaultAlert("Glaives", "Text", nil, 6, 1, encID)
-        timers = {
+        local timers = {
             [15] = {38, 108, 178}
         }
         self:AddRemindersFromTable(Alert, timers[id])
 
         local Alert = self:CreateDefaultAlert("Interrupts", "Text", nil, 6, 1, encID)
-        timers = {
+        local timers = {
             [15] = {59, 129}
         }
         self:AddRemindersFromTable(Alert, timers[id])
 
         local Alert = self:CreateDefaultAlert("Soaks", "Text", nil, 7, 3, encID)
         Alert.TTS = false
-        timers = {
+        local timers = {
             [15] = {20, 50, 80}
         }
         self:AddRemindersFromTable(Alert, timers[id])
 
         local Alert = self:CreateDefaultAlert("Orbs", "Text", nil, 7, 3, encID)
-        timers = {
+        local timers = {
             [15] = {35.5, 65.5, 95.5}
         }
         self:AddRemindersFromTable(Alert, timers[id])
 
         local Alert = self:CreateDefaultAlert("Crystal", "Text", nil, 5, 4, encID)
-        timers = {
+        local timers = {
             [15] = {22, 60, 98}
         }
         self:AddRemindersFromTable(Alert, timers[id])
 
         local Alert = self:CreateDefaultAlert("Soaks", "Text", nil, 5, 4, encID)
         Alert.text = "Soaks"
-        timers = {
+        local timers = {
             [15] = {31, 69, 107}
         }
         self:AddRemindersFromTable(Alert, timers[id])
