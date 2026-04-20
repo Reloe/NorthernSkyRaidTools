d2a1ed05877ddc71daa82b81d8334297665006cb
Reloe <Jerome.Bellefroid@gmail.com>
2026-04-20 21:00:40 +0200

    Update MidnightFalls.lua

--- EncounterAlerts/MidnightS1/MidnightFalls.lua
+++ EncounterAlerts/MidnightS1/MidnightFalls.lua
@@ -427,7 +427,7 @@ NSI.DetectPhaseChange[encID] = function(self, e, info)
     local difficultyID = select(3, GetInstanceInfo()) or 0
     if (not difficultyID) or (not detectedDurations[difficultyID]) then return end
     local phaseinfo = detectedDurations[difficultyID][self.Phase]
-    if phaseinfo and info.duration == phaseinfo.time then
+    if phaseinfo and ApproximatelyEqual(info.duration, phaseinfo.time, 0.2) then
         local newphase = phaseinfo.phase(self.Phase)
         if newphase > self.Phase then
             self.Phase = newphase
