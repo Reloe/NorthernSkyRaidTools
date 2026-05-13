b96a365cb2a5c3f1b8fec4563485657b820984cb
Reloe <Jerome.Bellefroid@gmail.com>
2026-05-13 22:30:00 +0200

    Update enUS.lua

--- Locales/enUS.lua
+++ Locales/enUS.lua
@@ -1,4 +1,4 @@
-local L = LibStub("AceLocale-3.0"):NewLocale("NorthernSkyRaidTools", "enUS", true, false)
+local L = LibStub("AceLocale-3.0"):NewLocale("NorthernSkyRaidTools", "enUS", true, true)
 
 if not L then return end
 
