1fea925f705b1ae8c0a14149ef56904663dff6e4
Reloe <Jerome.Bellefroid@gmail.com>
2026-05-15 15:03:52 +0200

    remove spellid for rotmire alerts

--- EncounterAlerts/MidnightS1/Rotmire.lua
+++ EncounterAlerts/MidnightS1/Rotmire.lua
@@ -3,13 +3,13 @@ local _, NSI = ... -- Internal namespace
 local encID = 3159
 -- /run NSAPI:DebugEncounter(3159)
 NSI.InitializeAlerts[encID] = function(self)
-    local data = {internalID = "Adds", text = "Adds", DisplayType = "Text", encID = encID, phase = 1, TTS = true, dur = 8, spellID = 1221622,
+    local data = {internalID = "Adds", text = "Adds", DisplayType = "Text", encID = encID, phase = 1, TTS = true, dur = 8, spellID = nil,
         timers = {
             [16] = {23, 72, 159, 208, 295, 344, 431, 480},
         },
     }
     self:AddEncounterAlert(data)
-    local data = {internalID = "Shrooms", text = "Shrooms", DisplayType = "Text", encID = encID, phase = 1, TTS = true, dur = 6, spellID = 1221637,
+    local data = {internalID = "Shrooms", text = "Shrooms", DisplayType = "Text", encID = encID, phase = 1, TTS = true, dur = 6, spellID = nil,
         timers = {
             [16] = {120, 256, 392, 528},
         },
