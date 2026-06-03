aed9a1eb0912f8f325d8ed55813ee1bd6666ba0e
Reloe <Jerome.Bellefroid@gmail.com>
2026-06-03 21:31:28 +0200

    add Rotmire Tank Alerts

--- EncounterAlerts/MidnightS1/Rotmire.lua
+++ EncounterAlerts/MidnightS1/Rotmire.lua
@@ -27,6 +27,38 @@ NSI.InitializeAlerts[encID] = function(self)
     end]],
     }
     self:AddEncounterAlert(data)
+    local data = {group = "Tanks", internalID = "Taunts", text = "Taunt", customIcon = 355, DisplayType = "Text", encID = encID, phase = 1, TTS = "Taunt", TTSTimer = 0, dur = 5, spellID = nil,
+    overrides =
+        {textColors = {0, 1, 0, 1},
+            isConditional = {
+                text = "This Alert only shows if you do not have threat on the Boss.",
+                func = [[return function() local threat = UnitThreatSituation("player", "boss1") return threat and threat < 2 end]],
+            },
+        },
+        timers = {
+            [16] = {24.5, 36.5, 48.5, 60.5, 73.5, 85.5, 97.5, 109.5,
+                    160.6, 172.6, 184.6, 196.6, 209.6, 221.6, 233.6, 245.6,
+                    296.6, 308.6, 320.6, 332.6, 345.6, 357.6, 369.6, 381.6,
+                    432.6, 444.6, 456.6, 468.6, 481.6, 493.6, 505.6, 507.6},
+        },
+    }
+    self:AddEncounterAlert(data)
+    local data = {group = "Tanks", internalID = "Tankhits", text = "Tank-Hit", customIcon = 134201, DisplayType = "Text", encID = encID, phase = 1, TTS = false, dur = 5, spellID = nil,
+    overrides =
+        {textColors = {1, 0, 0, 1},
+            isConditional = {
+                text = "This Alert only shows if you have threat on the Boss.",
+                func = [[return function() local threat = UnitThreatSituation("player", "boss1") return threat and threat >= 2 end]],
+            },
+        },
+        timers = {
+            [16] = {26, 38, 50, 62, 75, 87, 99, 111,
+                    162.1, 174.1, 186.1, 198.1, 211.1, 223.1, 235.1, 247.1,
+                    298.1, 310.1, 322.1, 334.1, 347.1, 359.1, 371.1, 383.1,
+                    434.1, 446.1, 458.1, 470.1, 483.1, 495.1, 507.1, 509.1},
+        },
+    }
+    self:AddEncounterAlert(data)
 end
 
 NSI.EncounterAlertStart[encID] = function(self) -- on ENCOUNTER_START
