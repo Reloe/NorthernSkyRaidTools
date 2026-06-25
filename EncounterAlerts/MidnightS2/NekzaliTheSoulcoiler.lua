8291635a3cd85824e0298923820b338ca4888124
Reloe <Jerome.Bellefroid@gmail.com>
2026-06-26 00:28:57 +0200

    add phase detection

--- EncounterAlerts/MidnightS2/NekzaliTheSoulcoiler.lua
+++ EncounterAlerts/MidnightS2/NekzaliTheSoulcoiler.lua
@@ -30,13 +30,24 @@ NSI.EncounterAlertStop[encID] = function(self) -- on ENCOUNTER_END
     self:EncounterRegister("NekzaliPhaseDetect", {"UNIT_SPELLCAST_START", "UNIT_SPELLCAST_SUCCEEDED"}, false)
 end
 
+local detectedDurations = {
+    [14] = { [1.5] = { time = 45, phase = function(num) return 2 end } },
+    [15] = { [1.5] = { time = 45, phase = function(num) return 2 end } },
+    [16] = { [1.5] = { time = 45, phase = function(num) return 2 end } },
+}
+
 NSI.DetectPhaseChange[encID] = function(self, e, info)
     local now = GetTime()
     if (not self.PhaseSwapTime) or (not self.EncounterID) or (not self.Phase) then return end
 
     if e ~= "ENCOUNTER_TIMELINE_EVENT_ADDED" or (not info) or (not (now > self.PhaseSwapTime + 5)) then return end
-    if self.Phase == 1.5 and info.duration == 45 then
-        self.Phase = 2
+    local difficultyID = self:DifficultyCheck({14, 15, 16})
+    if (not difficultyID) or (not detectedDurations[difficultyID]) then return end
+    local phaseinfo = detectedDurations[difficultyID][self.Phase]
+    if phaseinfo and info.duration == phaseinfo.time then
+        local newphase = phaseinfo.phase(self.Phase)
+        if newphase <= self.Phase then return end
+        self.Phase = newphase
         self:StartReminders(self.Phase)
         self.PhaseSwapTime = now
     end
--- EncounterAlerts/MidnightS2/TheLostExplorers.lua
+++ EncounterAlerts/MidnightS2/TheLostExplorers.lua
@@ -6,3 +6,25 @@ local encID = 3497
 NSI.InitializeAlerts[encID] = function(self)
     NSRT.EncounterAlerts[encID] = NSRT.EncounterAlerts[encID] or {}
 end
+
+local detectedDurations = {
+    [14] = { { time = 60, phase = function(num) return num + 1 end } },
+    [15] = { { time = 60, phase = function(num) return num + 1 end } },
+    [16] = { { time = 60, phase = function(num) return num + 1 end } },
+}
+
+NSI.DetectPhaseChange[encID] = function(self, e, info)
+    local now = GetTime()
+    if e ~= "ENCOUNTER_TIMELINE_EVENT_ADDED" or (not info) or (not self.PhaseSwapTime) or (not (now > self.PhaseSwapTime + 5)) or (not self.EncounterID) or (not self.Phase) then return end
+    local difficultyID = self:DifficultyCheck({14, 15, 16})
+    if (not difficultyID) or (not detectedDurations[difficultyID]) then return end
+    local phaseinfo = detectedDurations[difficultyID][1]
+    if phaseinfo and info.duration == phaseinfo.time then
+        local newphase = phaseinfo.phase(self.Phase)
+        if newphase > self.Phase then
+            self.Phase = newphase
+            self:StartReminders(self.Phase)
+            self.PhaseSwapTime = now
+        end
+    end
+end
