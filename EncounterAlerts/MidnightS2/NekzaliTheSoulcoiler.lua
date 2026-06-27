c7ff3c3c2a4c1a8789f2434104fb37cb068e00a9
Reloe <Jerome.Bellefroid@gmail.com>
2026-06-27 15:42:27 +0200

    add initial alerts

--- EncounterAlerts/MidnightS2/NekzaliTheSoulcoiler.lua
+++ EncounterAlerts/MidnightS2/NekzaliTheSoulcoiler.lua
@@ -5,6 +5,64 @@ local encID = 3470
 
 NSI.InitializeAlerts[encID] = function(self)
     NSRT.EncounterAlerts[encID] = NSRT.EncounterAlerts[encID] or {}
+    local nonTankConditions = self:DefaultLoadConditions()
+    nonTankConditions.Roles.DAMAGER = true
+    nonTankConditions.Roles.HEALER = true
+
+    local data = {group = "Nek'zali", internalID = "Barrage", name = "Barrage", text = "Frontal", DisplayType = "Text", encID = encID, phase = {1, 2}, TTS = false, dur = 8,
+        textColors = {1, 0, 0, 1}, customIcon = 1284103,
+        phaseTimers = {
+            [15] ={
+                {35.1, 64.2, 116.9, 146, 198.8, 227.9},
+                {51.1, 101.1, 151.1, 200.1}
+            },
+            [16] ={
+                {35.1, 64.2, 116.9, 146, 198.8, 227.9},
+                {51.1, 101.1, 151.1, 200.1}
+            }
+        },
+    }
+    self:AddEncounterAlert(data)
+
+    local data = {group = "Nek'zali", internalID = "Debuffs", name = "Essence Rend", text = "Debuffs", DisplayType = "Text", encID = encID, phase = 1, TTS = false, dur = 8,
+        loadConditions = nonTankConditions, customIcon = 1287434,
+        timers = {
+            [15] = {18.3, 76.5, 100.1, 158.3, 181.9},
+            [16] = {18.3, 76.5, 100.1, 158.3, 181.9},
+        },
+    }
+    self:AddEncounterAlert(data)
+
+    local data = {group = "Nek'zali", internalID = "SoulcoilIgnition", name = "Soulcoil Ignition", text = "AoE", DisplayType = "Text", encID = encID, phase = 1, TTS = false, dur = 8,
+        loadConditions = nonTankConditions, customIcon = 1293664,
+        timers = {
+            [15] = {85.5, 167.35, 249.2},
+            [16] = {85.5, 167.35, 249.2},
+        },
+    }
+    self:AddEncounterAlert(data)
+
+    local data = {group = "Nek'zali", internalID = "RestlessAmani", name = "Add-Spawn", text = "Adds", DisplayType = "Text", encID = encID, phase = {1, 2}, TTS = false, dur = 8, customIcon = 1295397,
+        timers = {
+            [15] = {
+                {45, 81.6, 118.3, 154.9, 191.5, 228.2},
+                {32, 82, 132, 182},
+            },
+            [16] = {
+                {45, 81.6, 118.3, 154.9, 191.5, 228.2},
+                {32, 82, 132, 182},
+            },
+        },
+    }
+    self:AddEncounterAlert(data)
+
+    local data = {group = "Nek'zali", internalID = "Invoke", name = "Invoke", text = "Dodge", DisplayType = "Text", encID = encID, phase = 2, TTS = false, dur = 8, customIcon = 1299673,
+        timers = {
+            [15] = {17, 45, 67, 95, 117, 145, 167, 195, 217},
+            [16] = {17, 45, 67, 95, 117, 145, 167, 195, 217},
+        },
+    }
+    self:AddEncounterAlert(data)
 end
 
 NSI.EncounterAlertStart[encID] = function(self) -- on ENCOUNTER_START
