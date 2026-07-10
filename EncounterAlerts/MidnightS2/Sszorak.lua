d3b5abe99db56c73c2aa43e1838d37619e5a5a3a
Reloe <Jerome.Bellefroid@gmail.com>
2026-07-11 01:33:03 +0200

    Update Sszorak.lua

--- EncounterAlerts/MidnightS2/Sszorak.lua
+++ EncounterAlerts/MidnightS2/Sszorak.lua
@@ -16,7 +16,7 @@ NSI.InitializeAlerts[encID] = function(self)
         textColors = {1, 0, 0, 1},
         timers = {
             [15] = {5.5, 55.7, 141.7, 195.9, 281.8, 334.1},
-            [16] = {5.5, 55.7, 141.7, 195.9, 281.8, 334.1},
+            [16] = {4.9, 52, 132, 179, 281.8, 334.1},
         },
     }
     self:AddEncounterAlert(data)
@@ -24,7 +24,7 @@ NSI.InitializeAlerts[encID] = function(self)
     local data = {group = "Sszorak", internalID = "DamageAmp", name = "Damage Amp", text = "Damage Amp", DisplayType = "Text", encID = encID, phase = 1, TTS = false, dur = 6, spellID = 1286033,
         timers = {
             [15] = {111.1, 249.3},
-            [16] = {111.1, 249.3},
+            [16] = {100, 249.3},
         },
     }
     self:AddEncounterAlert(data)
@@ -33,7 +33,7 @@ NSI.InitializeAlerts[encID] = function(self)
         loadConditions = tankConditions,
         timers = {
             [15] = {32.2, 84.4, 170.4, 222.6, 308.5, 360.8},
-            [16] = {32.2, 84.4, 170.4, 222.6, 308.5, 360.8},
+            [16] = {28.8, 76.8, 156, 203, 308.5, 360.8},
         },
     }
     self:AddEncounterAlert(data)
@@ -41,7 +41,7 @@ NSI.InitializeAlerts[encID] = function(self)
     local data = {group = "Sszorak", internalID = "WindDebuffs", text = "Wind-Debuffs", DisplayType = "Text", encID = encID, phase = 1, TTS = false, dur = 6, spellID = 1285419,
         timers = {
             [15] = {43.4, 95.5, 181.5, 233.7, 319.7, 371.9},
-            [16] = {43.4, 95.5, 181.5, 233.7, 319.7, 371.9},
+            [16] = {39.7, 86.7, 166.8, 233.7, 319.7, 371.9},
         },
     }
     self:AddEncounterAlert(data)
@@ -49,14 +49,15 @@ NSI.InitializeAlerts[encID] = function(self)
         loadConditions = nontankConditions,
         timers = {
             [15] = {37.2, 89.5, 175.4, 227.6, 313.5, 365.8},
-            [16] = {37.2, 89.5, 175.4, 227.6, 313.5, 365.8},
+            [16] = {32, 79.8, 159, 227.6, 313.5, 365.8},
         },
     }
     self:AddEncounterAlert(data)
 
-    local data = {group = "Sszorak", internalID = "SerpentsFury", name = "Serpent's Fury", text = "Soak", DisplayType = "Text", encID = encID, phase = 1, TTS = false, dur = 6,
+    local data = {group = "Sszorak", internalID = "SerpentsFury", name = "Serpent's Fury", text = "Stack Up", DisplayType = "Text", encID = encID, phase = 1, TTS = "Stack", dur = 6,
+        loadConditions = nontankConditions,
         timers = {
-            [16] = {},
+            [16] = {22, 74, 150, 202},
         },
     }
     self:AddEncounterAlert(data)
