a10276e30037dc3a51062a9cc04ffafbbf830423
Sanxy09 <unosovago1991@gmail.com>
2026-07-22 14:02:36 +0800

    Add localization support for Phase in Note-Display

--- Reminders.lua
+++ Reminders.lua
@@ -411,7 +411,7 @@ function NSI:ProcessReminder()
             end)
             for _, data in ipairs(remindertable) do
                 if not phasedisplayed[data.phase] then
-                    data.str = "Phase "..data.phase.."\n"..data.str
+                    data.str = NSI:Loc("Phase").. " " .. data.phase.."\n"..data.str
                     phasedisplayed[data.phase] = true
                 end
                 self.DisplayedReminder = self.DisplayedReminder..data.str.."\n"
@@ -428,7 +428,7 @@ function NSI:ProcessReminder()
             end)
             for _, data in ipairs(personalremindertable) do
                 if not phasedisplayed[data.phase] then
-                    data.str = "Phase "..data.phase.."\n"..data.str
+                    data.str = NSI:Loc("Phase").. " " .. data.phase.."\n"..data.str
                     phasedisplayed[data.phase] = true
                 end
                 self.DisplayedPersonalReminder = self.DisplayedPersonalReminder..data.str.."\n"
@@ -1260,7 +1260,7 @@ function NSI:CountdownNoteFrame(frame)
     if not originalText:match('\n$') then originalText = originalText..'\n' end
     for line in originalText:gmatch('([^\n]*)\n') do
         local ShouldDelete = false
-        local phase = line:match("Phase (%d*%.?%d+)")
+        local phase = line:match(NSI:Loc("Phase").." (%d*%.?%d+)")
         curphase = phase and tonumber(phase) or curphase
         if curphase < self.Phase then
             ShouldDelete = true
