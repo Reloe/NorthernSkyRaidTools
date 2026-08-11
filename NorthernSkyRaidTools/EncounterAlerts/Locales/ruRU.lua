8b11c0153634939f6cd56f9b736e60deb2d7000d
Reloe <Jerome.Bellefroid@gmail.com>
2026-08-11 15:29:07 +0200

    add placeholders for all other currently supported languages

---
+++ NorthernSkyRaidTools/EncounterAlerts/Locales/ruRU.lua
@@ -0,0 +1,212 @@
+local _, NSI = ...
+
+NSI.EncounterAlertLocales = NSI.EncounterAlertLocales or {}
+local L = {}
+NSI.EncounterAlertLocales["ruRU"] = L
+
+L[3176] = {
+    -- ["Soaks"] = {text = "", name = "", group = ""},
+}
+
+L[3177] = {
+    -- ["Breath"] = {text = "", name = "", group = ""},
+    -- ["Knock"] = {text = "", name = "", group = ""},
+}
+
+L[3179] = {
+    -- ["CC Display"] = {text = "", name = "", group = ""},
+    -- ["Beams"] = {text = "", name = "", group = ""},
+    -- ["CC Adds"] = {text = "", name = "", group = ""},
+    -- ["Orbs"] = {text = "", name = "", group = ""},
+}
+
+L[3178] = {
+    -- ["Breath"] = {text = "", name = "", group = ""},
+    -- ["HealthDisplay"] = {text = "", name = "", group = ""},
+    -- ["Spread"] = {text = "", name = "", group = ""},
+    -- ["Tether"] = {text = "", name = "", group = ""},
+}
+
+L[3180] = {
+    -- ["Aura of Wrath"] = {text = "", name = "", group = ""},
+    -- ["TauntAlerts"] = {text = "", name = "", group = ""},
+    -- ["Heal Absorb Ticks"] = {text = "", name = "", group = ""},
+    -- ["Peace Aura"] = {text = "", name = "", group = ""},
+    -- ["Sacred Toll"] = {text = "", name = "", group = ""},
+    -- ["Devotion Aura"] = {text = "", name = "", group = ""},
+}
+
+L[3181] = {
+    -- ["Tether"] = {text = "", name = "", group = ""},
+    -- ["Bait_P1"] = {text = "", name = "", group = ""},
+    -- ["Bait_P3"] = {text = "", name = "", group = ""},
+    -- ["Bait_P5"] = {text = "", name = "", group = ""},
+    -- ["Explosion_P1"] = {text = "", name = "", group = ""},
+    -- ["Explosion_P3"] = {text = "", name = "", group = ""},
+    -- ["Explosion_P5"] = {text = "", name = "", group = ""},
+    -- ["Arrows"] = {text = "", name = "", group = ""},
+    -- ["Ranged Obelisk_P1"] = {text = "", name = "", group = ""},
+    -- ["Ranged Obelisk_P3"] = {text = "", name = "", group = ""},
+    -- ["Ranged Obelisk_P5"] = {text = "", name = "", group = ""},
+    -- ["Boss-Immune"] = {text = "", name = "", group = ""},
+    -- ["Melee Obelisk_P1"] = {text = "", name = "", group = ""},
+    -- ["Melee Obelisk_P3"] = {text = "", name = "", group = ""},
+    -- ["Melee Obelisk_P5"] = {text = "", name = "", group = ""},
+    -- ["Stop Cast"] = {text = "", name = "", group = ""},
+}
+
+L[3306] = {
+    -- ["Debuffs_P1"] = {text = "", name = "", group = ""},
+    -- ["Debuffs_P2"] = {text = "", name = "", group = ""},
+}
+
+L[3182] = {
+    -- ["Feather Color"] = {text = "", name = "", group = ""},
+    -- ["Soaks_P1"] = {text = "", name = "", group = ""},
+    -- ["Soaks_P2"] = {text = "", name = "", group = ""},
+    -- ["Color Swap"] = {text = "", name = "", group = ""},
+    -- ["Next Hit_P2"] = {text = "", name = "", group = ""},
+    -- ["Next Hit_P3"] = {text = "", name = "", group = ""},
+    -- ["Quills_P1"] = {text = "", name = "", group = ""},
+    -- ["Quills_P2"] = {text = "", name = "", group = ""},
+    -- ["Gateway_P2"] = {text = "", name = "", group = ""},
+    -- ["Gateway_P3"] = {text = "", name = "", group = ""},
+}
+
+L[3183] = {
+    -- ["HC Soaks"] = {text = "", name = "", group = ""},
+    -- ["Right Stars"] = {text = "", name = "", group = ""},
+    -- ["Left Memory Game"] = {text = "", name = "", group = ""},
+    -- ["Right Soak-Time"] = {text = "", name = "", group = ""},
+    -- ["Left Stars"] = {text = "", name = "", group = ""},
+    -- ["Lura Tank-Hits_P4"] = {text = "", name = "", group = ""},
+    -- ["Spread"] = {text = "", name = "", group = ""},
+    -- ["Transition Beams"] = {text = "", name = "", group = ""},
+    -- ["Orbs"] = {text = "", name = "", group = ""},
+    -- ["Soak Cross"] = {text = "", name = "", group = ""},
+    -- ["Right Soaks"] = {text = "", name = "", group = ""},
+    -- ["MemoryGame"] = {text = "", name = "", group = ""},
+    -- ["InterruptDisplay"] = {text = "", name = "", group = ""},
+    -- ["Glaives"] = {text = "", name = "", group = ""},
+    -- ["Interrupts"] = {text = "", name = "", group = ""},
+    -- ["Old-Seed-Drop"] = {text = "", name = "", group = ""},
+    -- ["Right Memory Game"] = {text = "", name = "", group = ""},
+    -- ["Left Soaks"] = {text = "", name = "", group = ""},
+    -- ["CrystalDropTimer"] = {text = "", name = "", group = ""},
+    -- ["Beams"] = {text = "", name = "", group = ""},
+    -- ["Galvanize"] = {text = "", name = "", group = ""},
+    -- ["Blazes"] = {text = "", name = "", group = ""},
+    -- ["P4 Move"] = {text = "", name = "", group = ""},
+    -- ["Move"] = {text = "", name = "", group = ""},
+    -- ["RunesDisplay"] = {text = "", name = "", group = ""},
+    -- ["Soak Skull"] = {text = "", name = "", group = ""},
+    -- ["Seed-Drop"] = {text = "", name = "", group = ""},
+    -- ["Left Soak-Time"] = {text = "", name = "", group = ""},
+    -- ["Lura Taunts_P1"] = {text = "", name = "", group = ""},
+    -- ["Lura Taunts_P3"] = {text = "", name = "", group = ""},
+    -- ["Full Blaze"] = {text = "", name = "", group = ""},
+    -- ["Lura Tank-Hits_P1"] = {text = "", name = "", group = ""},
+    -- ["Lura Tank-Hits_P3"] = {text = "", name = "", group = ""},
+    -- ["Soak Star"] = {text = "", name = "", group = ""},
+    -- ["Final Slice Stars"] = {text = "", name = "", group = ""},
+    -- ["Soak Orange"] = {text = "", name = "", group = ""},
+}
+
+L[3159] = {
+    -- ["BurstingPustules"] = {text = "", name = "", group = ""},
+    -- ["Shrooms"] = {text = "", name = "", group = ""},
+    -- ["InterruptDisplay"] = {text = "", name = "", group = ""},
+    -- ["Taunts"] = {text = "", name = "", group = ""},
+    -- ["Tankhits"] = {text = "", name = "", group = ""},
+    -- ["Adds"] = {text = "", name = "", group = ""},
+}
+
+L[3379] = {
+}
+
+L[3470] = {
+    -- ["RestlessAmani"] = {text = "", name = "", group = ""},
+    -- ["Barrage"] = {text = "", name = "", group = ""},
+    -- ["HungeringPyre"] = {text = "", name = "", group = ""},
+    -- ["Debuffs"] = {text = "", name = "", group = ""},
+    -- ["SoulcoilIgnition"] = {text = "", name = "", group = ""},
+    -- ["InvokeMythic"] = {text = "", name = "", group = ""},
+    -- ["Invoke"] = {text = "", name = "", group = ""},
+}
+
+L[3445] = {
+    -- ["BloodSoakPool"] = {text = "", name = "", group = ""},
+    -- ["BloodHits"] = {text = "", name = "", group = ""},
+    -- ["BloodDispels"] = {text = "", name = "", group = ""},
+    -- ["TransitionDebuffs"] = {text = "", name = "", group = ""},
+    -- ["PoisonHits"] = {text = "", name = "", group = ""},
+    -- ["ShiftingProtovenom"] = {text = "", name = "", group = ""},
+    -- ["OrbSpawn"] = {text = "", name = "", group = ""},
+    -- ["BloodDropPool"] = {text = "", name = "", group = ""},
+    -- ["PoisonAdd"] = {text = "", name = "", group = ""},
+    -- ["BloodSoak"] = {text = "", name = "", group = ""},
+}
+
+L[3455] = {
+    -- ["Taunts"] = {text = "", name = "", group = ""},
+    -- ["Adds"] = {text = "", name = "", group = ""},
+    -- ["TankHits"] = {text = "", name = "", group = ""},
+    -- ["Infection"] = {text = "", name = "", group = ""},
+    -- ["WaveSpread"] = {text = "", name = "", group = ""},
+    -- ["Waves"] = {text = "", name = "", group = ""},
+    -- ["Soaks"] = {text = "", name = "", group = ""},
+    -- ["AoE"] = {text = "", name = "", group = ""},
+}
+
+L[3497] = {
+    -- ["MushroomJump"] = {text = "", name = "", group = ""},
+    -- ["ShreddingShards"] = {text = "", name = "", group = ""},
+    -- ["Fish-Spawn"] = {text = "", name = "", group = ""},
+    -- ["FrostfireVolley"] = {text = "", name = "", group = ""},
+    -- ["ShellSpinScroll"] = {text = "", name = "", group = ""},
+    -- ["MushroomBait"] = {text = "", name = "", group = ""},
+    -- ["BlinkNova"] = {text = "", name = "", group = ""},
+    -- ["ShellSpinTrader"] = {text = "", name = "", group = ""},
+    -- ["TimeToThrowNonConditional"] = {text = "", name = "", group = ""},
+    -- ["TimeToThrow"] = {text = "", name = "", group = ""},
+    -- ["ShellSpinNormal"] = {text = "", name = "", group = ""},
+    -- ["ExplosiveSurprise"] = {text = "", name = "", group = ""},
+    -- ["MightyThud"] = {text = "", name = "", group = ""},
+}
+
+L[3420] = {
+    -- ["DamageAmp"] = {text = "", name = "", group = ""},
+    -- ["Debuffs"] = {text = "", name = "", group = ""},
+    -- ["WindDebuffs"] = {text = "", name = "", group = ""},
+    -- ["TankCombo"] = {text = "", name = "", group = ""},
+    -- ["Bait"] = {text = "", name = "", group = ""},
+    -- ["WindsHelper"] = {text = "", name = "", group = ""},
+    -- ["SerpentsFury"] = {text = "", name = "", group = ""},
+}
+
+L[3421] = {
+    -- ["Adds"] = {text = "", name = "", group = ""},
+    -- ["Soak"] = {text = "", name = "", group = ""},
+    -- ["TankSoak"] = {text = "", name = "", group = ""},
+    -- ["PreSpread"] = {text = "", name = "", group = ""},
+    -- ["WatchSide"] = {text = "", name = "", group = ""},
+    -- ["Orbs"] = {text = "", name = "", group = ""},
+    -- ["WatchSpawns"] = {text = "", name = "", group = ""},
+    -- ["Defensives"] = {text = "", name = "", group = ""},
+    -- ["Knock"] = {text = "", name = "", group = ""},
+}
+
+L[3429] = {
+    -- ["InterruptAdds"] = {text = "", name = "", group = ""},
+    -- ["P2Taunt"] = {text = "", name = "", group = ""},
+    -- ["P2Frontal"] = {text = "", name = "", group = ""},
+    -- ["P1Soak"] = {text = "", name = "", group = ""},
+    -- ["P2Shield"] = {text = "", name = "", group = ""},
+    -- ["MindControls"] = {text = "", name = "", group = ""},
+    -- ["P2Debuffs"] = {text = "", name = "", group = ""},
+    -- ["P1Taunt"] = {text = "", name = "", group = ""},
+    -- ["P1Frontal"] = {text = "", name = "", group = ""},
+}
+
+L[3492] = {
+}
