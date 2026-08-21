bc62ca87b537dd496e355dedc26f31b30391e38e
Sanxy09 <unosovago1991@gmail.com>
2026-08-21 11:51:09 +0800

    Update zhCN.lua

--- NorthernSkyRaidTools/EncounterAlerts/Locales/zhCN.lua
+++ NorthernSkyRaidTools/EncounterAlerts/Locales/zhCN.lua
@@ -232,7 +232,7 @@ L[3421] = {
 -- The Coiled Altar (3429)
 L[3429] = {
     ["P1Frontal"]       = {group = "盘卷祭坛 P1", name = "撕裂", text = "正面顺劈"},
-    ["P1OrbDeadline"]   = {group = "盘卷祭坛 P1", name = "搬球倒计时", text = "搬球倒计时"},
+    ["P1OrbDeadline"]   = {group = "盘卷祭坛 P1", name = "搬球时限", text = "搬球时限"},
     ["P1Taunt"]         = {group = "盘卷祭坛 坦克", name = "P1 嘲讽", text = "嘲讽"},
     ["P1Soak"]          = {group = "盘卷祭坛 P1", name = "处斩（分摊）", text = "分摊"},
     ["MindControls"]    = {group = "盘卷祭坛 P2", name = "恐惧行军（心控）", text = "心控"},
