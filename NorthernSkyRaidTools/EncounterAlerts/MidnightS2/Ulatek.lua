dc234f300355ad90c27975cd75dbfbf2e5768e13
Reloe <Jerome.Bellefroid@gmail.com>
2026-08-23 03:55:39 +0200

    reverse fill overview bar

--- NorthernSkyRaidTools/EncounterAlerts/MidnightS2/Ulatek.lua
+++ NorthernSkyRaidTools/EncounterAlerts/MidnightS2/Ulatek.lua
@@ -124,8 +124,8 @@ NSI.InitializeAlerts[encID] = function(self)
     local data = {group = "Ula'tek", internalID = "GraspingFangsOverview", name = "Grasping Fangs Overview", text = nil, DisplayType = "Bar", encID = encID, phase = 1, TTS = false, dur = 40,
         spellID = 1311611, id = 0.2, difficulties = {15, 16}, isSpecialDisplay = true, BlockCopy = true, NoEdit = true, Preview = UlatekGraspingFangsPreview, enabled = false,
         timers = {
-            [15] = {189},
-            [16] = {189},
+            [15] = {195},
+            [16] = {195},
         },
     }
     self:AddEncounterAlert(data)
