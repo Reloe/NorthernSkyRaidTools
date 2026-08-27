d364cde81940291c41c2f9a3d5c928cd549ec690
Reloe <Jerome.Bellefroid@gmail.com>
2026-08-28 01:08:21 +0200

    Update Sszorak.lua

--- NorthernSkyRaidTools/EncounterAlerts/MidnightS2/Sszorak.lua
+++ NorthernSkyRaidTools/EncounterAlerts/MidnightS2/Sszorak.lua
@@ -87,8 +87,8 @@ NSI.InitializeAlerts[encID] = function(self)
     }
     self:AddEncounterAlert(data)
 
-    local data = {group = "Sszorak", internalID = "SerpentsFury", name = "Serpent's Fury", text = "Stack Up", DisplayType = "Text", encID = encID, phase = 1, TTS = "Stack", dur = 6,
-        loadConditions = nontankConditions,
+    local data = {Version = {versionNumber = 1, [1] = {loadConditions = {}}}, group = "Sszorak", internalID = "SerpentsFury", name = "Serpent's Fury", text = "Stack Up", DisplayType = "Text", encID = encID, phase = 1, TTS = "Stack", dur = 6,
+        loadConditions = {},
         timers = {
             [16] = {25.5, 75.5, 152, 202.5, 279.5, 329.5},
         },
