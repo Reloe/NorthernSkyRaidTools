266e5edf0ea305021fbf97b4f1da1efef8dd64b4
Elnarfim <elnarfim@yahoo.com>
2026-08-28 18:25:31 +0900

    Update koKR

--- NorthernSkyRaidTools/EncounterAlerts/Locales/koKR.lua
+++ NorthernSkyRaidTools/EncounterAlerts/Locales/koKR.lua
@@ -209,7 +209,7 @@ L[3497] = {
 -- Sszorak (3420)
 L[3420] = {
     ["TankCombo"]   = {group = "스조라크", name = "탱커 연속 공격", text = "탱커 연속 공격"},
-    -- ["SetMarkers"]  = {group = "Sszorak", name = "Mark Reminder", text = "Set Markers"},
+    ["SetMarkers"]  = {group = "스조라크", name = "바닥징 리마인더", text = "바닥징 찍기"},
     ["DamageAmp"]   = {group = "스조라크", name = "약화됨", text = "약화됨"},
     ["Bait"]        = {group = "스조라크", name = "유도", text = "유도"},
     ["WindDebuffs"] = {group = "스조라크", name = "바람 디버프", text = "바람 디버프"},
